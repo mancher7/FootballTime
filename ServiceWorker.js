e051aae11c7a4e944829abb5914e29307ab330a3
MANCHER7 <man.unit7@mail.ru>
2025-02-23 14:33:00 +0300

    обнова

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,6 +1,33 @@
+const cacheName = "DefaultCompany-Football Time 2.0-0.1";
+const contentToCache = [
+    "Build/FootballTime.loader.js",
+    "Build/FootballTime.framework.js.gz",
+    "Build/FootballTime.data.gz",
+    "Build/FootballTime.wasm.gz",
+    "TemplateData/style.css"
+
+];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
 });
 
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
